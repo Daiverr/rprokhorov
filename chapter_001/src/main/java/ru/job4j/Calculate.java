d17123509f09aef473b34eeb1f414ccdc434c129
Roman Prokhorov <daiverr1@gmail.com>
2018-01-06 18:23:35 +0300

    1.4. JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,27 @@
 package ru.job4j;
 
-public class Calculate{
- public static void main(String[] args){
-    System.out.println("Hello World");
+/**
+ * Calculate.
+ *
+ * @author Roman Prokhorov (daiverr1@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Calculate {
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
     }
 }
---
+++ chapter_001/src/test/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate test.
+*
+* @author Roman Prokhorov (daiverr1@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
