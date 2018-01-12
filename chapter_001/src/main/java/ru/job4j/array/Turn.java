045fe855ff01c39e99f844909a597326a2d83e5b
Roman Prokhorov <daiverr1@gmail.com>
2018-01-12 18:50:37 +0300

    Перевернуть массив. Рефакторинг

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -9,18 +9,10 @@ package ru.job4j.array;
 
 public class Turn {
     public int[] back(int[] array) {
-        if (array.length % 2 == 0) {
-            for (int i = 0; i < (array.length / 2); i++) {
-                int buffer = array[i];
-                array[i] = array[array.length - 1 - i];
-                array[array.length - 1 - i] = buffer;
-            }
-        } else {
-            for (int i = 0; i < ((array.length - 1) / 2); i++) {
-                int buffer = array[i];
-                array[i] = array[array.length - 1 - i];
-                array[array.length - 1 - i] = buffer;
-            }
+        for (int i = 0; i < ((array.length - array.length % 2) / 2); i++) {
+            int buffer = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = buffer;
         }
         return array;
     }
