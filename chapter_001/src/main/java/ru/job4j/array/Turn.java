6a36e50e528d81c4619a1bcbce9e2f7fca71bcc4
Roman Prokhorov <daiverr1@gmail.com>
2018-01-13 21:15:44 +0300

    factoring

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -9,7 +9,7 @@ package ru.job4j.array;
 
 public class Turn {
     public int[] back(int[] array) {
-        for (int i = 0; i < ((array.length - array.length % 2) / 2); i++) {
+        for (int i = 0; i < array.length / 2; i++) {
             int buffer = array[i];
             array[i] = array[array.length - 1 - i];
             array[array.length - 1 - i] = buffer;
