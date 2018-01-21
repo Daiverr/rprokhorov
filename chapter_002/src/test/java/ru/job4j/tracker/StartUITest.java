29e9abea7741e8d15d62a8cc45305abb2934ad5c
Roman Prokhorov <daiverr1@gmail.com>
2018-01-21 18:54:39 +0300

    Доработка тестов в StartUITest проверяющие вывод данных в консоль.

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,5 +1,11 @@
 package ru.job4j.tracker;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -30,6 +36,48 @@ public class StartUITest {
         Input input = new StubInput(new String[]{"3", item.getId(), "6"});
         new StartUI(input, tracker).init();
         assertThat(tracker.getAll().length, is(1));
+    }
+
+    PrintStream stdout = System.out;
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+    }
+
+    @Test
+    public void whenGetAllItems() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "desc", 6));
+        Input input = new StubInput(new String[]{"1", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(new String(out.toByteArray()), containsString("Заявка: " + item.getId() + " " + item.getName()));
+    }
+
+    @Test
+    public void whenFindByID() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "desc", 6));
+        tracker.add(new Item("test name2", "desc", 7));
+        Input input = new StubInput(new String[]{"4", item.getId(), "6"});
+        new StartUI(input, tracker).init();
+        assertThat(new String(out.toByteArray()), containsString(item.getName() + "\n" + item.getDesc()));
+    }
+
+    @Test
+    public void whenFindByName() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "desc", 6));
+        tracker.add(new Item("test name", "desc22", 7));
+        Input input = new StubInput(new String[]{"5", item.getName(), "6"});
+        new StartUI(input, tracker).init();
+        assertThat(new String(out.toByteArray()), containsString(item.getDesc()));
+        assertThat(new String(out.toByteArray()), containsString("desc22"));
     }
 
 }
