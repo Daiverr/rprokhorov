c92039b384af7400e1814e45d9c10c90dafde07c
Roman Prokhorov <daiverr1@gmail.com>
2018-03-22 15:17:39 +0300

    Очередь с приоритетом на LinkedList.

---
+++ chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -0,0 +1,33 @@
+package ru.job4j.search;
+/**
+ * @author Roman Prokhorov (daiverr1@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    /**
+     * Метод должен вставлять в нужную позицию элемент.
+     * Позиция определять по полю приоритет.
+     * Для вставик использовать add(int index, E value)
+     * @param task задача
+     */
+    public void put(Task task) {
+        Iterator<Task> iterator = tasks.listIterator();
+        if (iterator.hasNext()) {
+            Task element = iterator.next();
+            if (element.getPriority() > task.getPriority()) {
+                tasks.add(tasks.indexOf(element), task);
+            }
+        } else tasks.add(task);
+    }
+
+    public Task take() {
+        return this.tasks.poll();
+    }
+}
---
+++ chapter_003/src/main/java/ru/job4j/search/Task.java
@@ -0,0 +1,24 @@
+package ru.job4j.search;
+/**
+ * @author Roman Prokhorov (daiverr1@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class Task {
+    private String desc;
+    private int priority;
+
+    public Task(String desc, int priority) {
+        this.desc = desc;
+        this.priority = priority;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
