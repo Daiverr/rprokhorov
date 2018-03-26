18a5e1b6103d2636aec5a419885afd8b598d3f7d
Roman Prokhorov <daiverr1@gmail.com>
2018-03-26 23:40:50 +0300

    Сортировка User с использованием Comparator.

--- chapter_003/src/main/java/ru/job4j/sort/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/sort/SortUser.java
@@ -1,17 +1,54 @@
 package ru.job4j.sort;
 
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class SortUser {
 
     public Set<User> sort (List<User> in) {
         Set<User> result= new TreeSet<>();
-        for (User user: in) {
-            result.add(user);
-        }
+        result.addAll(in);
+        return result;
+    }
 
+    public List<User> sortNameLength (List<User> in) {
+        //необходимо определить Comparator для метода Collections.sort и отсортировать List<User> по длине имени.
+        List<User> result = new ArrayList<User>();
+        result.addAll(in);
+        Collections.sort(result, new UserComparatorLength());
+        return result;
+    }
+
+    public List<User> sortByAllFields (List<User> in) {
+       // отсортировать List<User> по обоим полям, сначала сортировка по имени в лексикографическом порядке, потом по возрасту
+        List<User> result = new ArrayList<User>();
+        result.addAll(in);
+        Collections.sort(result, new UserComparatorAlhрabetThanAge());
         return result;
     }
 }
+
+class UserComparatorLength implements Comparator<User> {
+
+    @Override
+    public int compare(User o1, User o2) {
+        return Integer.compare(o1.getName().length(), o2.getName().length());
+
+    }
+}
+
+class UserComparatorAlhрabetThanAge implements Comparator<User> {
+
+    @Override
+    public int compare(User o1, User o2) {
+        int result;
+        if (o1.getName().compareTo(o2.getName()) == 0) {
+            result = Integer.compare(o1.getAge(), o2.getAge());
+        } else result = o1.getName().compareTo(o2.getName());
+        return result;
+    }
+}
+
+
+
+
+
--- chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
@@ -4,19 +4,43 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class SortUserTest {
 
     @Test
-    public void mnb() {
+    public void whenCompareByAge() {
         List<User> users = new ArrayList<>();
         users.add(new User("Roman", 28));
         users.add(new User("Vasya", 23));
         SortUser su = new SortUser();
         Set<User> usersSort = su.sort(users);
         assertThat(usersSort.iterator().next().getAge(), is(23));
+    }
+
+    @Test
+    public void whenCompareByNameLength() {
+        List<User> users = new ArrayList<>();
+        users.add(new User("Vladimir", 23));
+        users.add(new User("Roman", 28));
+        SortUser su = new SortUser();
+        List<User> usersSort = su.sortNameLength(users);
+        assertThat(usersSort.iterator().next().getName(), is("Roman"));
+    }
+
+    @Test
+    public void whenCompareByAllFields() {
+        List<User> users = new ArrayList<>();
+        users.add(new User("Vladimir", 23));
+        users.add(new User("Roman", 28));
+        users.add(new User("Vladimir", 31));
+        users.add(new User("Roman", 19));
+        SortUser su = new SortUser();
+        List<User> usersSort = su.sortByAllFields(users);
+        Iterator<User> iterator = usersSort.iterator();
+        assertThat(iterator.next().getName(), is("Roman"));
+        assertThat(iterator.next().getAge(), is(28));
+        assertThat(iterator.next().getName(), is("Vladimir"));
+
     }
 }
